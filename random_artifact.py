ddd7a2dafe44382ac3a22217a740f006a7b456a9
butterski <miloszkucharski@gmail.com>
2021-03-27 18:08:20 +0100

    generowanie modyfikatora cen + optymalizacja kodu

--- random_artifact.py
+++ random_artifact.py
@@ -2,14 +2,24 @@ import json
 import random
 
 
-def generate_random_artifact(how_many):
+with open('artifacts.json') as artifact_file:  # open file as data variable
+    data = json.load(artifact_file)
+
+
+def price_modify():
+    price_modifier = random.randrange(-100, 100)
+    return price_modifier
+
+
+def generate_random_artifact(price_modifier):
     if __name__ == "__main__":
-        with open('artifacts.json') as artifact_file:  # open file as data variable
-            data = json.load(artifact_file)
+        random_artifact = random.choice(data["artifacts"])   # save items informations
+        price = random_artifact["price"] + price_modifier
+        if price < 1:
+            price = random_artifact["price"]
+        print(f'ID: {random_artifact["id"]}, Name: {random_artifact["name"]}, Price: {price}')  # prints item id and name
 
-        for i in range(how_many):
-            random_artifact = random.choice(data["artifacts"])
-            print(f'ID: {random_artifact["id"]}, Name: {random_artifact["name"]}')
 
-
-generate_random_artifact(3)
+for i in range(5):
+    price_modificator = price_modify()
+    generate_random_artifact(price_modificator)

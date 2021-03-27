d94cfad7589af2fd9d073effd30303d89e7150e2
butterski <miloszkucharski@gmail.com>
2021-03-27 16:09:13 +0100

    komentarze

--- artifact_show.py
+++ artifact_show.py
@@ -3,11 +3,11 @@ import json
 
 def print_artifacts():
     if __name__ == "__main__":
-        with open('artifacts.json') as artifact_file:
+        with open('artifacts.json') as artifact_file:               # open file as data variable
           data = json.load(artifact_file)
 
         print("List of all Artifacts:")
-        for item in data["artifacts"]:
+        for item in data["artifacts"]:                              # prints all artifacts id and name
             print(f'ID: {item["id"]}, Name: {item["name"]} ')
 
 

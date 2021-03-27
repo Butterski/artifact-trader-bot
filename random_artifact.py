569334ecdcbc37d103faa3d55109a924c5958c8a
butterski <miloszkucharski@gmail.com>
2021-03-27 18:53:39 +0100

    price + rarity support

--- random_artifact.py
+++ random_artifact.py
@@ -6,11 +6,6 @@ with open('artifacts.json') as artifact_file:  # open file as data variable
     data = json.load(artifact_file)
 
 
-def price_modify():
-    price_modifier = random.randrange(-100, 100)
-    return price_modifier
-
-
 """
 1-40 - common
 41-70 - rare
@@ -19,9 +14,16 @@ def price_modify():
 """
 
 
-def generate_random_artifact(price_modificator):
+def generate_random_artifact():
     if __name__ == "__main__":
+        def price_modifier(price):
+            mod_range = price * 0.35
+            price_modify = random.randrange((mod_range * -1), mod_range)
+            price = price + price_modify
+            return price
+
         rarity = random.randrange(1, 100)
+
         if 1 <= rarity <= 40:
             random_artifact = random.choice(data["artifacts"]["rarity"]["common"])
 
@@ -34,18 +36,10 @@ def generate_random_artifact(price_modificator):
         elif 91 <= rarity <= 100:
             random_artifact = random.choice(data["artifacts"]["rarity"]["legendary"])
 
-        price = random_artifact["price"] + price_modificator
-        if price < 1:
-            price = random_artifact["price"]
+        price = random_artifact["price"]
+        price = price_modifier(price)
         print(f'Name: {random_artifact["name"]}, Price: {price}')  # prints item id and name
-
-        """
-        random_artifact = random.choice(data["artifacts"])   # save items informations
-        
-        print(f'ID: {random_artifact["id"]}, Name: {random_artifact["name"]}, Price: {price}')  # prints item id and name
-        """
 
 
 for i in range(100):
-    price_modificator = price_modify()
-    generate_random_artifact(price_modificator)
+    generate_random_artifact()
